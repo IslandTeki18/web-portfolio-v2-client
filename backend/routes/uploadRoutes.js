5e83c1a126b9d67caf051350838bd3a1ab3bac0e
Landon McKell <landon.roney7923@gmail.com>
2021-02-12 23:50:01 -0700

    aws s3 image functionality

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,41 +1,12 @@
-import path from "path";
 import express from "express";
-import multer from "multer";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
+import { uploadImages } from "../middleware/uploadMiddleware.js";
+import { uploadProductImage } from "../controllers/uploadControler.js";
+
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
-
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  } else {
-    cb("Images only!");
-  }
-}
-
-const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-});
-
-router.post("/", upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
-});
+router
+  .route("/")
+  .post(protect, isAdmin, uploadImages.single("image"), uploadProductImage);
 
 export default router;
