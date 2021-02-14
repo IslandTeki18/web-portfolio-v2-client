c81815af1fa1fb43e4f31771e430224376cbf517
Landon McKell <landon.roney7923@gmail.com>
2021-02-13 19:30:04 -0700

    s3 implementation v2.0

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { upload } from "../middleware/uploadMiddleware.js";
+import { uploadProductImage } from "../controllers/uploadController.js";
 
 const router = express.Router();
 
-router.route("/").post(upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
-});
+router.route("/").post(upload.single("image"), uploadProductImage);
 
 export default router;
