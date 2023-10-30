80280e4a86fbafc19d7eb40a30f7fb259f6f1fc3
Landon McKell <landon.roney7923@gmail.com>
2023-10-29 22:39:17 -0600

    SideNav Fixes
    DESC
    - Updated design to be mobile responsive
    - Updated functionality of SideNav

--- src/features/admin/components/sideNav/SideNav.tsx
+++ src/features/admin/components/sideNav/SideNav.tsx
@@ -1,54 +1,68 @@
 import * as React from "react";
-import { Fragment, useState } from "react";
+import { useState, Fragment } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
-  BellIcon,
-  CalendarIcon,
-  ChartPieIcon,
   Cog6ToothIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
   HomeIcon,
-  UsersIcon,
   XMarkIcon,
-} from "@heroicons/react/24/outline";
-
-import {
+  FolderIcon,
   ChevronDownIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/20/solid";
+  UserIcon,
+  InboxStackIcon,
+} from "@heroicons/react/24/outline";
+import { useSetRecoilState } from "recoil";
+import { userRoleState } from "~src/stores";
+import { Link, useNavigate } from "react-router-dom";
+import { classNames } from "../../utils";
 
 type SideNavProps = {
   // Define component props here
 };
 
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
-
-const navigation = [
-  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
-];
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-];
-const userNavigation = [
-  { name: "Your profile", href: "#" },
-  { name: "Sign out", href: "#" },
-];
-
 export const SideNav = (props: SideNavProps) => {
+  const navigate = useNavigate();
+  const setUserRole = useSetRecoilState(userRoleState);
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const navigation = [
+    {
+      name: "Dashboard",
+      href: "/admin/dashboard",
+      icon: HomeIcon,
+      current: true,
+    },
+    {
+      name: "Projects",
+      href: "/admin/projects",
+      icon: FolderIcon,
+      current: false,
+    },
+    {
+      name: "Contacts",
+      href: "/admin/contacts",
+      icon: InboxStackIcon,
+      current: false,
+    },
+  ];
+  const userNavigation = [
+    { name: "Your Profile", onClick: onProfileClickHandler },
+    { name: "Logout", onClick: onLogoutHandler },
+  ];
+
+  function onProfileClickHandler() {
+    return navigate("/admin/profile");
+  }
+
+  function onLogoutHandler() {
+    setUserRole("user");
+    localStorage.clear();
+    return navigate("/", {
+      replace: true,
+    });
+  }
+
   return (
-    <>
+    <div>
       <Transition.Root show={sidebarOpen} as={Fragment}>
         <Dialog
           as="div"
@@ -101,7 +115,6 @@ export const SideNav = (props: SideNavProps) => {
                     </button>
                   </div>
                 </Transition.Child>
-                {/* Sidebar component, swap this element with another sidebar if you like */}
                 <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
                   <div className="flex h-16 shrink-0 items-center">
                     <img
@@ -116,8 +129,8 @@ export const SideNav = (props: SideNavProps) => {
                         <ul role="list" className="-mx-2 space-y-1">
                           {navigation.map((item) => (
                             <li key={item.name}>
-                              <a
-                                href={item.href}
+                              <Link
+                                to={item.href}
                                 className={classNames(
                                   item.current
                                     ? "bg-gray-800 text-white"
@@ -130,47 +143,23 @@ export const SideNav = (props: SideNavProps) => {
                                   aria-hidden="true"
                                 />
                                 {item.name}
-                              </a>
+                              </Link>
                             </li>
                           ))}
                         </ul>
                       </li>
-                      <li>
-                        <div className="text-xs font-semibold leading-6 text-gray-400">
-                          Your teams
-                        </div>
-                        <ul role="list" className="-mx-2 mt-2 space-y-1">
-                          {teams.map((team) => (
-                            <li key={team.name}>
-                              <a
-                                href={team.href}
-                                className={classNames(
-                                  team.current
-                                    ? "bg-gray-800 text-white"
-                                    : "text-gray-400 hover:text-white hover:bg-gray-800",
-                                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                )}
-                              >
-                                <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                  {team.initial}
-                                </span>
-                                <span className="truncate">{team.name}</span>
-                              </a>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
+
                       <li className="mt-auto">
-                        <a
-                          href="#"
-                          className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
+                        <button
+                          onClick={onLogoutHandler}
+                          className="w-full group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                         >
                           <Cog6ToothIcon
                             className="h-6 w-6 shrink-0"
                             aria-hidden="true"
                           />
-                          Settings
-                        </a>
+                          Logout
+                        </button>
                       </li>
                     </ul>
                   </nav>
@@ -181,17 +170,9 @@ export const SideNav = (props: SideNavProps) => {
         </Dialog>
       </Transition.Root>
 
-      {/* Static sidebar for desktop */}
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
-          <div className="flex h-16 shrink-0 items-center">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
-          </div>
+          <div className="flex h-4 shrink-0 items-center"></div>
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li>
@@ -206,6 +187,7 @@ export const SideNav = (props: SideNavProps) => {
                             : "text-gray-400 hover:text-white hover:bg-gray-800",
                           "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                         )}
+                        onClick={() => {}}
                       >
                         <item.icon
                           className="h-6 w-6 shrink-0"
@@ -217,47 +199,95 @@ export const SideNav = (props: SideNavProps) => {
                   ))}
                 </ul>
               </li>
-              <li>
-                <div className="text-xs font-semibold leading-6 text-gray-400">
-                  Your teams
-                </div>
-                <ul role="list" className="-mx-2 mt-2 space-y-1">
-                  {teams.map((team) => (
-                    <li key={team.name}>
-                      <a
-                        href={team.href}
-                        className={classNames(
-                          team.current
-                            ? "bg-gray-800 text-white"
-                            : "text-gray-400 hover:text-white hover:bg-gray-800",
-                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                        )}
-                      >
-                        <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                          {team.initial}
-                        </span>
-                        <span className="truncate">{team.name}</span>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </li>
               <li className="mt-auto">
-                <a
-                  href="#"
-                  className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
+                <button
+                  onClick={onLogoutHandler}
+                  className="w-full group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                 >
                   <Cog6ToothIcon
                     className="h-6 w-6 shrink-0"
                     aria-hidden="true"
                   />
-                  Settings
-                </a>
+                  Logout
+                </button>
               </li>
             </ul>
           </nav>
         </div>
       </div>
-    </>
+
+      <div className="lg:pl-72">
+        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-gray-600 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+
+          <div
+            className="h-6 w-px bg-gray-900/50 lg:hidden"
+            aria-hidden="true"
+          />
+
+          <div className="flex flex-1 justify-end gap-x-4 self-stretch lg:gap-x-6">
+            <div className="flex items-center gap-x-4 lg:gap-x-6">
+              <div
+                className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
+                aria-hidden="true"
+              />
+
+              <Menu as="div" className="relative">
+                <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                  <span className="sr-only">Open user menu</span>
+                  <UserIcon className="h-6 w-6" />
+                  <span className="hidden lg:flex lg:items-center">
+                    <span
+                      className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                      aria-hidden="true"
+                    >
+                      Landon McKell
+                    </span>
+                    <ChevronDownIcon
+                      className="ml-2 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Menu.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    {userNavigation.map((item) => (
+                      <Menu.Item key={item.name}>
+                        {({ active }) => (
+                          <button
+                            onClick={item.onClick}
+                            className={classNames(
+                              active ? "bg-gray-50" : "",
+                              "block px-3 py-1 text-sm leading-6 text-gray-900"
+                            )}
+                          >
+                            {item.name}
+                          </button>
+                        )}
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/features/admin/routes/dashboard/Dashboard.tsx
+++ src/features/admin/routes/dashboard/Dashboard.tsx
@@ -7,7 +7,9 @@ export const Dashboard = () => {
       <SideNav />
       <div className="lg:pl-72">
         <main className="py-10">
-          <div className="px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="px-4 sm:px-6 lg:px-8">
+            <div className="bg-danger-500 h-screen w-full"></div>
+          </div>
         </main>
       </div>
     </div>
