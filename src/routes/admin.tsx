d184ff17309c2d03a85c60a5779b0a3a01a7786a
Landon McKell <landon.roney7923@gmail.com>
2023-12-01 08:46:17 -0700

    potential routing fix

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,18 +1,19 @@
 import * as React from "react";
 
-import { Dashboard, ProjectDetails, ProjectList } from "~/src/features";
+import { Dashboard, ProjectDetails, ProjectList } from "~/src/features/admin";
 
 export const adminRoutes = [
   {
     path: "/admin/dashboard",
     element: <Dashboard />,
+    index: true,
   },
   {
     path: "/admin/project/:id",
-    element: <ProjectDetails />
+    element: <ProjectDetails />,
   },
   {
     path: "/admin/projects-list",
-    element: <ProjectList />
-  }
+    element: <ProjectList />,
+  },
 ];
