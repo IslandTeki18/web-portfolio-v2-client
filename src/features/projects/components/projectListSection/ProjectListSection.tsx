aa10087d534413e705461d7c04e285dd3bf613d8
Landon McKell <landon.roney7923@gmail.com>
2024-08-25 22:41:43 -0600

    chore: Refactor ProjectListSection component and fetch projects from API

--- src/features/projects/components/projectListSection/ProjectListSection.tsx
+++ src/features/projects/components/projectListSection/ProjectListSection.tsx
@@ -1,21 +1,27 @@
 import * as React from "react";
-import { PROJECTS } from "~src/data";
 import { SectionWrapper } from "~src/components";
-import { ProjectCard } from "../projectCard";
-import { formatDate } from "~src/utils";
+import { ProjectImageCard } from "../projectImageCard";
+import { useFetch } from "~src/hooks";
+
+// @ts-ignore
+import noProjectImage from "~src/features/projects/assets/noImage2.png?as=webp";
 
 export const ProjectListSection = () => {
-  
+  const { data: projects, loading, error } = useFetch("/projects");
+
   function renderProjects() {
-    if (!Array.isArray(PROJECTS)) return;
-    return PROJECTS.map((project) => (
-      <ProjectCard
-        key={project.id}
-        id={project.id}
+    if (loading) return <p className="text-white">Loading...</p>;
+    if (error) return <p className="text-white">Error: {error.message}</p>;
+    if (!Array.isArray(projects)) return;
+    return projects.map((project) => (
+      <ProjectImageCard
+        key={project._id}
+        id={project._id}
         title={project.title}
         projectDescription={project.description}
         projectType={project.applicationType}
-        date={formatDate(project.createdAt, "en-US")}
+        projectImage={project.images[0] || noProjectImage}
+        createdAt={project.createdAt}
       />
     ));
   }
