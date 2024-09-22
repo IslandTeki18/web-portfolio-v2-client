04c795427ccf00a20d15e648a9422ece7ab21246
Landon McKell <landon.roney7923@gmail.com>
2024-09-22 09:18:53 -0600

    refactor: Update URLs in config to use environment variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const PRODUCTION_URL =
   process.env.PRODUCTION_URL || "http://localhost:5000/api/";
-export const NODE_ENV = process.env.REACT_APP_VERCEL_ENV || "development";
+export const NODE_ENV = process.env.NODE_ENV || "development";
 export const DEVELOPMENT_URL =
   process.env.DEVELOPMENT_URL || "http://localhost:5000/api/";
